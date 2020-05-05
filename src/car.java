af1765a075c9f58cc20d0fa2270ae949f670938e
johnpherson <phersonj@gmail.com>
2020-05-04 21:14:01 -0600

    fixed car class

--- src/car.java
+++ src/car.java
@@ -2,20 +2,30 @@ import java.awt.*;
 
 public class Car {
 
-    double AverageMilesPerGallon;
+    // Data Types:
+    // int -> integer 1, 2, 3
+    // double -> decimal 34.5, 32.1
+    // String -> "a1a2" or "Hello World"
+    // Color -> from awt library
+    // boolean -> true or false
+
+    double averageMilesPerGallon;
     String licensePlate;
     Color paintColor;
     boolean areTailingWorking;
 
-
     public Car(double inputAverageMPG,
                String inputLicensePlate,
-               Color inputpaintColor,
+               Color inputPaintColor,
                boolean inputAreTaillightsWorking) {
         this.averageMilesPerGallon = inputAverageMPG;
         this.licensePlate = inputLicensePlate;
-        this.paintColor = inputpaintColor;
+        this.paintColor = inputPaintColor;
         this.areTailingWorking = inputAreTaillightsWorking;
+    }
+
+    public void changePaintColor(Color newPaintColor) {
+        this.paintColor = newPaintColor;
     }
 
 }
