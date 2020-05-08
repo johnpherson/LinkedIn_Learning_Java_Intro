c3b111beb11a6252210f32e9258b178ec551a20d
johnpherson <phersonj@gmail.com>
2020-05-08 16:56:13 -0600

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,48 @@
 import javax.xml.namespace.QName;
+import java.lang.reflect.Array;
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        // 31, 45, 22, 98, 10
+        int[] numbers = new int[5];
+
+        numbers[0] = 31;
+        numbers[1] = 45;
+        numbers[2] = 22;
+        numbers[3] = 98;
+        numbers[4] = 10;
+
+        int[] numbers2 = {31, 45, 22, 98, 10};
+        Arrays.sort(numbers2);
+        System.out.println(Arrays.toString(numbers2));
+
+        String[] myFavoriteCandyBars = {"Twix", "Snickers", "Heath"};
+        System.out.println("Index 2: " + myFavoriteCandyBars[2]);
+        myFavoriteCandyBars[2] = "Butterfinger";
+        System.out.println("Index 2: " + myFavoriteCandyBars[2]);
+        System.out.println("Length: "+myFavoriteCandyBars.length);
+
+        String candy2 = myFavoriteCandyBars[2];
+        int candy2length = candy2.length();
+        System.out.println("Index 2 length is: "+candy2length);
+
+        Array.get(myFavoriteCandyBars, 2);
 
 
-//        System.out.println("Enter a word: ");
+        /*
+        System.out.println("Enter a number: ");
         Scanner sc = new Scanner(System.in);
         int useNumber = sc.nextInt();
+
         System.out.println(useNumber);
         double userNumber2 = sc.nextDouble();
         System.out.println(userNumber2);
 
-        /*
+
         String userInput = sc.next();
 
         String uppercased = userInput.toUpperCase();
@@ -28,3 +56,6 @@ public class Main {
 
     }
 }
+
+
+
