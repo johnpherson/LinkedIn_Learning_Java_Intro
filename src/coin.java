9d9237607823c979e830ddb9360cf92819984990
johnpherson <phersonj@gmail.com>
2020-05-13 11:52:13 -0600

    whiel loops and coins

---
+++ src/coin.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class coin {
+
+    public final static int HEADS = 0;
+    public final static int TAILS = 1;
+
+    private int faceUp;
+
+    public coin(int start) {
+        faceUp = start;
+    }
+
+}
